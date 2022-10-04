330a480e5e58a359354ce2c18db27d529ddf373c
thiago-santanna <uzmaki_thiago@outlook.com>
2022-10-04 16:27:39 -0300

    teste login falso OK

--- src/modules/User/services/LoginUserService.test.ts
+++ src/modules/User/services/LoginUserService.test.ts
@@ -29,7 +29,26 @@ describe("Ao fazer o login do usuario", () => {
     expect(macth).toBe(true);
   });
 
-  // it("quando as senhas não corresponder deve ser falso", () => {
+  it("quando as senhas não corresponder deve ser falso", async () => {
+    const userRepository = new UserMemoryRepositpory();
+    const encrypter = new BcryptProvider();
+    const createUserService = new CreateUserService(userRepository, encrypter);
+    const loginUserService = new LoginUserService(userRepository, encrypter);
 
-  // });
+    const usuario: IUserRequest = {
+      nome: "John",
+      email: "john@gmail.com",
+      whatsApp: "81999999999",
+      cidade: "recife",
+      estado: "PE",
+      password: "102030",
+    };
+    const userResturned = await createUserService.execute(usuario);
+
+    const email = userResturned.email;
+    const password = "1020";
+
+    const macth = await loginUserService.execute({ email, password });
+    expect(macth).toBe(false);
+  });
 });
