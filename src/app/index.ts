6ed02a9d875dcdd6f741e752af045031c37ff22f
thiago-santanna <uzmaki_thiago@outlook.com>
2022-10-04 17:50:52 -0300

    Add Swagger Doc

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 import routes from "../routers/index";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../../swagger.json";
+
 const Application = express();
 Application.use(express.json());
 Application.use(cors());
+Application.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 Application.use(routes);
 
 export default Application;
